07f421e4d975b39eecbdbf2387d1a31a2efdc284
madhu nepal <madhunepal@gmail.com>
2017-10-10 23:20:38 +0000

    first commit for OOP Bank

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,11 @@
+require './bank_account'
+describe BankAccount do
+it "is created with an opening balance and the name of the clint"
+it "can report it's balance"
+it "can make deposites"
+it "can make withdrawals"
+it "can transfer funds to another account"
+it "has a minimum opening balance"
+it "allows the user to change the minimum balance"
+end
+
