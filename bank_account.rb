c6d019c2813a9b68bda7824371ffd661f0e8d07a
madhu nepal <madhunepal@gmail.com>
2017-10-13 03:38:59 +0000

    maintaining minimum balance feature is added

--- bank_account.rb
+++ bank_account.rb
@@ -1,7 +1,25 @@
 class BankAccount
  attr_reader :balance
+    @@minimum_opening_balance = 200
 
-  def initialize(opening_balance, account_holder)
-   @balance = opening_balance
-end
+   def initialize(opening_balance, account_holder)
+       if opening_balance < @@minimum_opening_balance
+           raise ArgumentError
+       end
+      @balance = opening_balance
+   end
+   def deposite(amount)
+       @balance += amount #total balance dep+open
+   end
+    def withdraw(amount)
+        @balance -= amount
+    end
+    def transfer(amount, account)
+        withdraw(amount)
+        account.deposite(amount)
+    end
+    def self.minimum_balance = amount
+        @@minimum_balance = amount
+    end
+        
 end
